5a7cf3756d66d3e29cdf33241115747752fffdb3
Andrew Cooper <andrew.evil.genius@gmail.com>
2015-02-06 22:42:35 +0200

    Removed unneeded navigation items.

--- home/home_navigation.php
+++ home/home_navigation.php
@@ -1,8 +1,8 @@
 <li><a href="index.php?page=campaigns" <?php echo checkActiveLink("campaigns", $nav); ?>>Campaigns</a></li>
 <li><a href="index.php?page=leads" <?php echo checkActiveLink("leads", $nav); ?>>Leads</a></li>
-<li><a href="index.php?page=messages" <?php echo checkActiveLink("messages", $nav); ?>>Messages</a></li>
-<li><a href="index.php?page=account" <?php echo checkActiveLink("account", $nav); ?>>My Account</a></li>
+<!--<li><a href="index.php?page=messages" <?php echo checkActiveLink("messages", $nav); ?>>Messages</a></li>
+<li><a href="index.php?page=account" <?php echo checkActiveLink("account", $nav); ?>>My Account</a></li> -->
 <?php if (checkRole("admin")): ?>
     <li><a href="index.php?page=users" <?php echo checkActiveLink("users", $nav); ?>>Users</a></li>
-    <li><a href="index.php?page=settings" <?php echo checkActiveLink("settings", $nav); ?>>Settings</a></li>
+    <!-- <li><a href="index.php?page=settings" <?php echo checkActiveLink("settings", $nav); ?>>Settings</a></li> -->
 <?php endif; ?>
